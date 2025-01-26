90d0be43b3fb94b19a6b172723b3f15871938779
Anna Valéria Makari <94723781+OnnaMcadva@users.noreply.github.com>
2025-01-26 23:54:35 +0100

    Add files via upload

--- cpp_module_00/Warlock.cpp
+++ cpp_module_00/Warlock.cpp
@@ -1,49 +1,41 @@
 #include "Warlock.hpp"
 
-Warlock::Warlock()
-{
-}
-
-Warlock::Warlock(const Warlock& obj)
-{
-    *this = obj;
-}
-
-Warlock& Warlock::operator=(const Warlock& rhs)
-{
-    if (this != &rhs) {
-        m_name = rhs.m_name;
-        m_title = rhs.m_title;
-    }
-    return *this;
-}
-
-Warlock::Warlock(const std::string& name, const std::string& title) : m_name(name), m_title(title)
-{
-    std::cout << m_name << ": This looks like another boring day." << std::endl;
-}
-
-Warlock::~Warlock()
-{
-    std::cout << m_name << ": My job here is done!" << std::endl;
-}
-
-const std::string& Warlock::getName() const
-{
-    return m_name;
-}
-
-const std::string& Warlock::getTitle() const
-{
-    return m_title;
-}
-
-void Warlock::setTitle(const std::string& str)
-{
-    m_title = str;
-}
-
-void Warlock::introduce() const
-{
-    std::cout << m_name << ": I am " << m_name << ", " << m_title << "!" << std::endl;
-}
+        Warlock::Warlock()
+        {}
+        Warlock::Warlock(const Warlock& obj)
+        {
+            *this = obj;
+        }
+        Warlock& Warlock::operator=(const Warlock& rhs)
+        {
+            if (this != &rhs)
+            {
+                m_name = rhs.m_name;
+                m_title = rhs.m_title;
+            }
+            return *this;
+        }
+        Warlock::Warlock(const std::string& name, const std::string& title) : m_name(name) , m_title(title)
+        {
+            std::cout << m_name << ": This looks like another boring day." << std::endl;
+        }
+        Warlock::~Warlock()
+        {
+            std::cout << m_name << ": My job here is done!" << std::endl;
+        }
+        const std::string& Warlock::getName() const
+        {
+            return m_name;
+        }
+        const std::string& Warlock::getTitle() const
+        {
+            return m_title;
+        }
+        void Warlock::setTitle(const std::string& str)
+        {
+            m_title = str;
+        }
+        void Warlock::introduce() const
+        {
+            std::cout << m_name << ": I am " << m_name << ", " << m_title << "!" << std::endl;
+        }
--- cpp_module_00/Warlock.hpp
+++ cpp_module_00/Warlock.hpp
@@ -1,20 +1,22 @@
 #pragma once
 #include <iostream>
+#include <string>
+#include <map>
 
-class Warlock {
+class Warlock
+{
+    private :
+        Warlock();
+        Warlock(const Warlock& obj);
+        Warlock& operator=(const Warlock& rhs);
+        std::string m_name;
+        std::string m_title;
 
-private:
-    Warlock();
-    Warlock(const Warlock& obj);
-    Warlock& operator=(const Warlock& rhs);
-    std::string m_name;
-    std::string m_title;
-
-public:
-    Warlock(const std::string& name, const std::string& title);
-    ~Warlock();
-    const std::string& getName() const;
-    const std::string& getTitle() const;
-    void setTitle(const std::string& str);
-    void introduce() const;
+    public :
+        Warlock(const std::string& name, const std::string& title);
+        ~Warlock();
+        const std::string& getName() const;
+        const std::string& getTitle() const;
+        void setTitle(const std::string& str);
+        void introduce() const;
 };
